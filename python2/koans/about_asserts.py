9bb82f8544ac57de1ab0b30ef87ea1c9f2f711e5
Vatsala Dorairajan <vatsala.dorairajan@gmail.com>
2013-09-25 23:56:31 +0530

    done solving about_asserts.py

--- python2/koans/about_asserts.py
+++ python2/koans/about_asserts.py
@@ -15,26 +15,26 @@ class AboutAsserts(Koan):
         #
         #   http://bit.ly/about_asserts
 
-        self.assertTrue(False)  # This should be true
+        self.assertTrue(True)  # This should be true
 
     def test_assert_with_message(self):
         """
         Enlightenment may be more easily achieved with appropriate messages.
         """
-        self.assertTrue(False, "This should be true -- Please fix this")
+        self.assertTrue(True, "This should be true -- Please fix this")
 
     def test_fill_in_values(self):
         """
         Sometimes we will ask you to fill in the values
         """
-        self.assertEqual(__, 1 + 1)
+        self.assertEqual(2, 1 + 1)
 
     def test_assert_equality(self):
         """
         To understand reality, we must compare our expectations against
         reality.
         """
-        expected_value = __
+        expected_value = 2
         actual_value = 1 + 1
         self.assertTrue(expected_value == actual_value)
 
@@ -42,7 +42,7 @@ class AboutAsserts(Koan):
         """
         Some ways of asserting equality are better than others.
         """
-        expected_value = __
+        expected_value = 2
         actual_value = 1 + 1
 
         self.assertEqual(expected_value, actual_value)
@@ -53,7 +53,7 @@ class AboutAsserts(Koan):
         """
 
         # This throws an AssertionError exception
-        assert False
+        assert True
 
     def test_that_sometimes_we_need_to_know_the_class_type(self):
         """
@@ -65,14 +65,14 @@ class AboutAsserts(Koan):
         # For example, contemplate the text string "naval". What is it's class type?
         # The koans runner will include this feedback for this koan:
         #
-        #   AssertionError: '-=> FILL ME IN! <=-' != <type 'str'>
+        #   AssertionError: str != <type 'str'>
         #
         # So "naval".__class__ is equal to <type 'str'>? No not quite. This
         # is just what it displays. The answer is simply str.
         #
         # See for yourself:
 
-        self.assertEqual(__, "naval".__class__) # It's str, not <type 'str'>
+        self.assertEqual(str, "naval".__class__) # It's str, not <type 'str'>
 
         # Need an illustration? More reading can be found here:
         #
